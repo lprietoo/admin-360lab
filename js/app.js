f26c0dff51ce115a1e458b8de6e9a59f44c17330
Laura Prieto <lprieto@gitei.edu.co>
2021-06-21 10:57:08 -0500

    updated

--- js/app.js
+++ js/app.js
@@ -1,50 +1,12 @@
 
 //---------------------Variable------------------//
+var user = firebase.auth().currentUser;
 var token;
-
-//-----------------Firestore---------------------//
-
-// Guardar tokens en las colecciones "laboratorios" y "tokens"
-function guardarToken() {
-
-  var fechaCreacion = moment().format('YYYY-MM-DD hh:mm') // guarda fecha de creación
-  var creacion = moment(fechaCreacion).unix() // convierte fecha a formato unix
-  var fechaExp = $('#datetime').val() // guarda fecha de expiración
-  var exp = moment(fechaExp, 'YYYY-MM-DD hh:mm').unix() // convierte fecha a formato unix
-// Asigna los tokens al laboratorio selecionado
-
-  // db.collection("laboratorios").doc(lab).set({ tokens:token})
-
-  db.collection("laboratorios")
-    .doc(lab)
-    .update({
-
-      tokens:firebase.firestore.FieldValue.arrayUnion(token)
-  })
-  .catch((error) => {
-    console.error("Error ", error);
-  });
-
-  db.collection("tokens").doc(token).set({
-
-      fechaCreacion: creacion,
-      fechaExp: exp,
-
-    }).then(function() {
-      $("#btnGuardarToken").addClass("d-none")
-      $("#tokenSucces").removeClass("d-none")
-    })  
-    .catch((error) => {
-      console.error("Error ", error);
-    });
-}
 
 
 
 //----------------Autenticacion-------------------//
 // Verifica el estado de autenticación del usuario
-
-var user = firebase.auth().currentUser;
 
 if (user == null) {
   var loc = location.pathname;
@@ -89,4 +51,41 @@ function logOut(){
 }
 
 
+
+//-----------------Firestore---------------------//
+
+// Guardar tokens en las colecciones "laboratorios" y "tokens"
+function guardarToken() {
+
+  var fechaCreacion = moment().format('YYYY-MM-DD hh:mm') // guarda fecha de creación
+  var creacion = moment(fechaCreacion).unix() // convierte fecha a formato unix
+  var fechaExp = $('#datetime').val() // guarda fecha de expiración
+  var exp = moment(fechaExp, 'YYYY-MM-DD hh:mm').unix() // convierte fecha a formato unix
+// Asigna los tokens al laboratorio selecionado
+
+  // db.collection("laboratorios").doc(lab).set({ tokens:token})
+
+  db.collection("laboratorios")
+    .doc(lab)
+    .update({
+
+      tokens:firebase.firestore.FieldValue.arrayUnion(token)
+  })
+  .catch((error) => {
+    console.error("Error ", error);
+  });
+
+  db.collection("tokens").doc(token).set({
+
+      fechaCreacion: creacion,
+      fechaExp: exp,
+
+    }).then(function() {
+      $("#btnGuardarToken").addClass("d-none")
+      $("#tokenSucces").removeClass("d-none")
+    })  
+    .catch((error) => {
+      console.error("Error ", error);
+    });
+}
 
