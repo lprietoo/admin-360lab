459ad2322a9c09b971f73648d8728216a121ac2f
Laura Prieto <lprieto@gitei.edu.co>
2021-06-25 06:40:12 -0500

    updated date format

--- js/app.js
+++ js/app.js
@@ -55,9 +55,8 @@ function observador() {
 function guardarToken() {
 
   var fechaCreacion = moment().format('YYYY-MM-DD hh:mm') // guarda fecha de creación
-  var creacion = moment(fechaCreacion).unix() // convierte fecha a formato unix
   var fechaExp = $('#datetime').val() // guarda fecha de expiración
-  var exp = moment(fechaExp, 'YYYY-MM-DD hh:mm').unix() // convierte fecha a formato unix
+  var exp = moment(fechaExp, 'YYYY-MM-DD hh:mm')
 // Asigna los tokens al laboratorio selecionado
 
   // db.collection("laboratorios").doc(lab).set({ tokens:token})
@@ -74,7 +73,7 @@ function guardarToken() {
 
   db.collection("tokens").doc(token).set({
 
-      fechaCreacion: creacion,
+      fechaCreacion: fechaCreacion,
       fechaExp: exp,
 
     }).then(function() {
